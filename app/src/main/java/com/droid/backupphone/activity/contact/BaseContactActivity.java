8670c99a8ce25d965da034f13d6a182a89993a7a
Nightchar <nikhilomar.bhumca08@gmail.com>
2017-05-01 18:58:25 +0530

    Sixth set of changes

--- app/src/main/java/com/droid/backupphone/activity/contact/BaseContactActivity.java
+++ app/src/main/java/com/droid/backupphone/activity/contact/BaseContactActivity.java
@@ -3,7 +3,10 @@ package com.droid.backupphone.activity.contact;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -43,6 +46,47 @@ public class BaseContactActivity extends BaseActivity {
         mLvContact = (ListView) findViewById(R.id.lv_contact);
         mTvNoData = (TextView) findViewById(R.id.tv_no_data);
         mLoadingProgress = findViewById(R.id.view_contact_progress);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_contact, menu);
+        final MenuItem selectAll = menu.findItem(R.id.action_select_all);
+        CheckBox cbSelectAll = (CheckBox) selectAll.getActionView().findViewById(R.id.action_item_checkbox);
+        if (cbSelectAll != null) {
+            // Set the text to match the item.
+            //cbSelectAll.setText(selectAll.getTitle());
+
+            // Add the onClickListener because the CheckBox doesn't automatically trigger onOptionsItemSelected.
+            cbSelectAll.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // update the menu item checked state based on checkbox checked state
+                    onOptionsItemSelected(selectAll.setChecked(((CheckBox) v).isChecked()));
+                }
+            });
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_select_all:
+                selectAllContact(item.isChecked());
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // select / deselect all contacts in listview.
+    private void selectAllContact(boolean checked) {
+        if (mLvContact.getVisibility() == View.VISIBLE && mLvContact.getCount() > 0) {
+            for (int i = 0; i < mLvContact.getChildCount(); i++) {
+                mLvContact.setItemChecked(i, checked);
+            }
+        }
     }
 
     // the method detail is given in subclasses.
